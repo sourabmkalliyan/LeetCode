ebf6727e3a27567bd98b28d97f976ac4d73766e0
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-01-20 23:35:54 +0530

    0125. Valid Palindrome - added 2nd solution

--- 0125. Valid Palindrome.py
+++ 0125. Valid Palindrome.py
@@ -1,4 +1,4 @@
-#Solution 1
+#Solution 1 - Using python in-build function and extra space
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
@@ -7,3 +7,24 @@ class Solution:
             if c.isalnum():
                 newstr += c.lower()
         return newstr == newstr[::-1]
+
+#Solution 2 - Using two pointer, O(1) space complexity
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not self.alphaNum(s[l]):
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l, r = l+1, r-1
+        return True
+
+
+    def alphaNum(self, c):
+        return (ord('A') <= ord(c) <= ord('Z') or
+        ord('a') <= ord(c) <= ord('z') or
+        ord('0') <= ord(c) <= ord('9'))
