a014cda1f26208617b145243560986db6d57a4d4
sourabmkalliyan <sourabmkalliyanofficial@gmail.com>
2024-01-20 23:06:19 +0530

    0125. Valid Palindrome

---
+++ 0125. Valid Palindrome.py
@@ -0,0 +1,9 @@
+#Solution 1
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newstr = ""
+        for c in s:
+            if c.isalnum():
+                newstr += c.lower()
+        return newstr == newstr[::-1]
